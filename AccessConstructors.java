8a3b5cf77ba5657310745b3f1249b624138d5bb4
Sudha <Deepanjali.mysore@gmail.com>
2025-04-05 00:17:46 +0530

    Java Programs

---
+++ AccessConstructors.java
@@ -0,0 +1,24 @@
+package AccessSpecifierCase1;
+//access specifier for all types for Constructors (includes para and non para)
+public class AccessConstructors 
+{
+	AccessConstructors()
+	{
+		
+	}
+	AccessConstructors(int a)
+	{
+		
+	}
+	AccessConstructors(double b)
+	{
+		
+	}	
+	
+	public static void main(String[] args) 
+	{
+		new AccessConstructors();
+
+	}
+
+}
---
+++ AccessStaticMethods.java
@@ -0,0 +1,31 @@
+package AccessSpecifierCase1;
+//access specifier for all types for static method (includes para and non para)
+public class AccessStaticMethods
+{
+
+	public static void add()
+	{
+		
+	}	
+	private static void sub()
+	{
+		
+	}
+	protected static void mul()
+	{
+		
+	}
+	static void div()
+	{
+		
+	}
+	public static void main(String[] args) 
+	{
+		add();
+		sub();
+		mul();
+		div();
+		
+	}
+
+}
